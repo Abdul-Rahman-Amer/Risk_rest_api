8505ed1bcd8cb03a551e8d27f00659cf67d1988e
Abdulrahman Amer <117536219+Abdul-Rahman-Amer@users.noreply.github.com>
2022-12-03 22:46:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
             db_user = 'root'
 
         if db_pass is None:
-            db_pass = '13L0ck22!Mjordan23!'
+            db_pass = 'password'
 
         if db_pool_size is None:
             db_pool_size = 5
